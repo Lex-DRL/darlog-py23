7e46e900b3e716e28c6a100472c3f3f0d659c2b1
Lex Darlog (DRL) <drl.i3x@gmail.com>
2022-07-30 14:34:15 +0400

    the package itself

---
+++ src/darlog_py23/__init__.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+"""
+A tiny compatibility module for cross-Python2/3 code.
+It's not a replacement for neither ``six`` nor ``__future__`` modules but is more of an extension to them.
+"""
+
+from .__dataclass_attrs import *
+from .__str import *
